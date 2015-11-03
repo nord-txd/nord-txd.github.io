9b9e08773e5dd8b1ecb3d96fe113a6b07c7c1d28
Norddin Chetbi <nord@txdlimited.co.uk>
2015-11-03 09:41:36 +0000

    inital

---
+++ js/index.js
@@ -0,0 +1,26 @@
+var sentance = [
+  "You complete and utter",
+  "You're a total",
+  "You absolute",
+  "Fuck off you"
+];
+
+var insults = [
+  "cunt",
+  "wanker",
+  "slag",
+  "shit",
+  "fucker",
+  "bitch",
+  "twat"
+];
+
+function randomFrom(array) {
+  return array[Math.floor(Math.random() * array.length)];
+}
+
+var randSentance = randomFrom(sentance);
+$('.sentance').text(randSentance);
+
+var randInsult = randomFrom(insults);
+$('.insult').text(randInsult);
